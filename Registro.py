63cdba6ef7b08f946e1d1a3983099e9279729ce1
JerónimoVásquezGonzález <142264512+JeronimoVasquez1307@users.noreply.github.com>
2023-10-23 01:32:41 -0500

    Update Registro.py

--- Registro.py
+++ Registro.py
@@ -5,7 +5,7 @@ import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
 # Leer el archivo CSV en un DataFrame
-url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTAfp6uFRC6PylYstGVXoXaEiXv8TTqS3BFcx_wVJ-BfQOILj9OUluNgQQx4Ba4-kujstzHgF71Pv6-/pub?output=csv"
+url = "https://raw.githubusercontent.com/JeronimoVasquez1307/prueba_login/main/usuarios.csv"
 df = pd.read_csv(url)
 
 # Título de la aplicación
@@ -29,20 +29,10 @@ if st.button("Registrar"):
     df = pd.concat([df, nuevo_df], ignore_index=True)
   
     
-    credenciales = 'baseusuarios-402906-cb3c6dfcc470.json'
-    scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
-    credentials = ServiceAccountCredentials.from_json_keyfile_name(credenciales, scope)
-    gc = gspread.authorize(credentials)
-    
-    # Abrir la hoja de cálculo en Google Sheets
-    gc_url = "https://docs.google.com/spreadsheets/d/1urfXFaV-Ve11mAQJ4xKDX-4k-UPrYgSwVCx57U2fSu4/edit#gid=1768604619"
-    worksheet = gc.open_by_url(gc_url).sheet1
-    
-    # Leer la copia local del archivo CSV
-    df_local = pd.read_csv("usuarios.csv")
-    
-    # Actualizar la hoja de cálculo en Google Sheets
-    worksheet.update([df_local.columns.values.tolist()] + df_local.values.tolist())
+    df = pd.concat([df, nuevo_df], ignore_index=True)
+
+    # Actualizar el archivo CSV en GitHub
+    df.to_csv(github_csv_url, index=False, header=True)
     st.success("Usuario registrado con éxito")
    
 
