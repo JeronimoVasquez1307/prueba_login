7c87b52e69500564d13e05c71b190e414d66fca1
JerónimoVásquezGonzález <142264512+JeronimoVasquez1307@users.noreply.github.com>
2023-10-23 01:35:09 -0500

    Update Registro.py

--- Registro.py
+++ Registro.py
@@ -26,13 +26,18 @@ if st.button("Registrar"):
     }
     nuevo_df = pd.DataFrame([nueva_fila])
     df = pd.concat([df, nuevo_df], ignore_index=True)
-  
-    
-    df = pd.concat([df, nuevo_df], ignore_index=True)
-
     # Actualizar el archivo CSV en GitHub
-    df.to_csv(url, index=False, header=True)
-    st.success("Usuario registrado con éxito")
+    df.to_csv("usuarios.csv", index=False, header=True)
+      
+    
+     # Realizar una solicitud HTTP para cargar los datos actualizados en GitHub
+    with open("usuarios.csv", "rb") as file:
+        response = requests.put(url, data=file, headers={'Content-Type': 'application/octet-stream'})
+
+    if response.status_code == 200:
+        st.success("Usuario registrado con éxito y archivo actualizado en GitHub")
+    else:
+        st.error("Error al actualizar el archivo en GitHub")
    
 
 # Mostrar los datos de usuario registrados en el archivo CSV
